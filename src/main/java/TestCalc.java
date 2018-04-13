0515db4035a013deeea5091f7d5545ae0c2edbb6
Maxime Duchene <duchene.maxime@outlook.fr>
2018-04-13 11:32:11 +0200

    Ajout de la javadoc et du test de division par zero

--- src/main/java/TestCalc.java
+++ src/main/java/TestCalc.java
@@ -5,39 +5,72 @@ import junit.framework.TestSuite;
 
 public class TestCalc {
 	
+	/**
+	 * On teste l'addition de base
+	 */
 	public void testAddition() {
 		assertTrue(Operation.addition(2, 3)==5);
 	}
 
+	/**
+	 * On teste l'addition avec un param négatif
+	 */
 	public void testAdditionNeg() {
 		assertTrue(Operation.addition(-6, 3)==-3);
 	}
 	
+	/**
+	 * On teste la soustraction avec un résultat positif
+	 */
 	public void testSoustraction() {
 		assertTrue(Operation.soustraction(3, 2)==1);
 	}
 
+	/**
+	 * On teste la soustraction avec une résultat négatif
+	 */
 	public void testSoustractionNeg() {
 		assertTrue(Operation.soustraction(2, 3)==-1);
 	}
 
+	/**
+	 * On teste la multiplication de deux positifs
+	 */
 	public void testMult() {
 		assertTrue(Operation.multiplication(2, 3)==6);
 	}
 
+	/**
+	 * On teste la multiplication avec un paramètre à zéro
+	 */
 	public void testMultiplicationZero() {
 		assertTrue(Operation.multiplication(0, 3)==0);
 	}
 
+	/**
+	 * On teste la multiplication avec un paramètre négatif
+	 */
 	public void testMultiplicationNeg() {
-		assertTrue(Operation.multiplication(2, 3)==5);
+		assertTrue(Operation.multiplication(-2, 3)==-6);
 	}
 	
+	/**
+	 * On teste une division avec deux paramètres positifs
+	 */
 	public void testDivision() {
 		assertTrue(Operation.division(6, 3)==2);
 	}
-
+	
+	/**
+	 * On teste la division avec le second paramètre à zéro
+	 */
 	public void testDivisionZero() {
-		assertTrue(Operation.division(5, 0)==5);
+		try {
+			Operation.division(4, 0);
+			assertTrue(false);
+		}
+		catch (Exception e){
+			assertTrue(true);
+		}
 	}
 }
