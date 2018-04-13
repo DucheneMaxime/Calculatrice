eab76d9571b929c10737b9277f3ddf6eb451a72a
Maxime Duchene <duchene.maxime@outlook.fr>
2018-04-13 10:57:25 +0200

    Séparation en différentes classes et méthodes

--- src/main/java/Calculatrice.java
+++ src/main/java/Calculatrice.java
@@ -1,82 +1,20 @@
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+
 
 public class Calculatrice {
 	
-	/**
-	 * 
-	 * @param Opérateur 1
-	 * @param Opérateur 2
-	 * @return La division des deux opérateurs
-	 */
-	public static int division(int a , int b) {
-		return a/b;
-		//TO DO tests + test /0
-	}
-	
-	/**
-	 * 
-	 * @param Opérateur 1
-	 * @param Opérateur 2
-	 * @return L'addition des deux opérateurs
-	 */
-	public static int addition(int a , int b) {
-		return a+b;
-		//TO DO tests
-	}
-	
-	/**
-	 * 
-	 * @param Opérateur 1
-	 * @param Opérateur 2
-	 * @return La multiplication des deux opérateurs
-	 */
-	public static int multiplication(int a , int b) {
-		return a*b;
-		//TO DO tests
-	}
-	
-	/**
-	 * 
-	 * @param Opérateur 1
-	 * @param Opérateur 2
-	 * @return La soustraction des deux opérateurs
-	 */
-	public static int soustraction(int a , int b) {
-		return a-b;
-		//TO DO tests
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What is the first number?");
-		int num1 = Integer.parseInt(sc.nextLine());
-		// TO DO Vérifier qu'il donne un int
+		int num1 = Operation.testInt(sc.nextLine());
 		System.out.println("What is the operator?");
-		String ope = sc.nextLine();
-		// TO DO Vérifier + ou - ou * ou /
+		String ope = Operation.testOpe(sc.nextLine());
 		System.out.println("What is your last number?");
-		int num2 = Integer.parseInt(sc.nextLine());
-		// TO DO Même tests que pour num1
+		int num2 = Operation.testInt(sc.nextLine());
 		
-		int res=0;
-		// We check what is the operator and call the right method
-		switch(ope) {
-		case ("+"):
-			res=addition(num1, num2);
-		break;
-		case("-"):
-			res=soustraction(num1, num2);
-		break;
-		case("*"):
-			res=multiplication(num1, num2);
-		break;
-		case("/"):
-			res=division(num1, num2);
-		break;
-		default:
-			System.out.println("Error");
-			break;
-		}
+		int res = Operation.CountRes(num1, num2, ope);
 		
 		//We print the result of the operation
 		System.out.println("The result of "+num1+" "+ope+" "+num2+" = "+res);
---
+++ src/main/java/Operation.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Operation {
+	
+	/**
+	 * 
+	 * @param Opérateur 1
+	 * @param Opérateur 2
+	 * @return La division des deux opérateurs
+	 */
+	public static int division(int a , int b) {
+		return a/b;
+		//TO DO tests + test /0
+	}
+	
+	/**
+	 * 
+	 * @param Opérateur 1
+	 * @param Opérateur 2
+	 * @return L'addition des deux opérateurs
+	 */
+	public static int addition(int a , int b) {
+		return a+b;
+		//TO DO tests
+	}
+	
+	/**
+	 * 
+	 * @param Opérateur 1
+	 * @param Opérateur 2
+	 * @return La multiplication des deux opérateurs
+	 */
+	public static int multiplication(int a , int b) {
+		return a*b;
+		//TO DO tests
+	}
+	
+	/**
+	 * 
+	 * @param Opérateur 1
+	 * @param Opérateur 2
+	 * @return La soustraction des deux opérateurs
+	 */
+	public static int soustraction(int a , int b) {
+		return a-b;
+		//TO DO tests
+	}
+	
+	public static int testInt(String test) {
+		Scanner sc = new Scanner(System.in);
+		/*while(!StringUtils.isNumeric(test)) {
+			System.out.println("Please write down a number.");
+			test = sc.nextLine();
+		}*/
+		return Integer.parseInt(test);
+	}
+	
+	/**
+	 * La méthode boucle tant que l'utilisateur ne rentre pas un des quatres opérateurs
+	 * @param String a tester
+	 * @return + - * ou / 
+	 */
+	public static String testOpe(String test) {
+		Scanner sc = new Scanner(System.in);
+		while(!(test.equals("/")) && !(test.equals("*")) && !(test.equals("+")) && !(test.equals("-"))) {
+			System.out.println("Please type a valid operator among the followings: + - * /");
+			test = sc.nextLine();
+		}
+		return test;
+	}
+	
+	public static int CountRes(int num1, int num2, String ope) {
+		// We check what is the operator and call the right method accordingly
+		switch(ope) {
+		case ("+"):
+			return addition(num1, num2);
+		case("-"):
+			return soustraction(num1, num2);
+		case("*"):
+			return multiplication(num1, num2);
+		case("/"):
+			return division(num1, num2);
+		default:
+			System.out.println("Error");
+			break;
+		}
+		return 0;
+	}
+
+
+}
